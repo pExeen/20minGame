cc52b500946d6677331a9d90c3676061fce3baa9
pexeen <dj22dj22dj@gmail.com>
2017-12-31 20:49:46 +0100

    forgot an indent

--- collision.lua
+++ collision.lua
@@ -1,6 +1,6 @@
 function collision()
 	if squares.player.x == width or squares.player.x == 0 - squareSize or squares.player.y == height or squares.player.y == 0 - squareSize or (squares.player.x == squares.target.x and squares.player.y == squares.target.y) then
-	lose()
+		lose()
 	end
 	if squares.movable.x == squares.target.x and squares.movable.y == squares.target.y then
 		win()
