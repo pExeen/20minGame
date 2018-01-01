a132afac827742b4ea849b843e5a9e7ffe00eb6d
pexeen <dj22dj22dj@gmail.com>
2018-01-01 04:15:40 +0100

    added colors into object and put some more spacing

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ function love.load()
 
 	squareSize = 100
 
-	squares = {player = {score = 0}, movable = {}, target = {}}
+	squares = {player = {score = 0, colors = {r = 150, g = 0, b = 0}}, movable = {colors = {r = 0, g = 150, b = 0}}, target = {colors = {r = 0, g = 0, b = 150}}}
 	randomSquares()
 end
 
@@ -19,16 +19,18 @@ function love.draw()
 		love.graphics.setNewFont(40)
 		love.graphics.print("You are the RED square.\nYour mission is to put the GREEN square in the BLUE one.\nThe only rules are to stay on the screen and to stay away from the BLUE square.")
 	else
-		love.graphics.setColor(255, 255, 255)
 		love.graphics.print("Score: "..squares.player.score)
 		love.graphics.setBackgroundColor(30, 0, 5)
 		love.graphics.setColor(0, 0, 0)
 		love.graphics.rectangle('fill', squareSize, squareSize, width - 2 * squareSize, height - 2 * squareSize)
-		love.graphics.setColor(150, 0, 0)
+
+		love.graphics.setColor(squares.player.colors.r, squares.player.colors.g, squares.player.colors.b)
 		love.graphics.rectangle("fill", squares.player.x, squares.player.y, squareSize, squareSize)
-		love.graphics.setColor(0, 150, 0)
+
+		love.graphics.setColor(squares.movable.colors.r, squares.movable.colors.g, squares.movable.colors.b)
 		love.graphics.rectangle("fill", squares.movable.x, squares.movable.y, squareSize, squareSize)
-		love.graphics.setColor(0, 0, 150)
+
+		love.graphics.setColor(squares.target.colors.r, squares.target.colors.g, squares.target.colors.b)
 		love.graphics.rectangle("fill", squares.target.x, squares.target.y, squareSize, squareSize)
 	end
 end
